a28cba840a6eaa3fec4d3b66d2812f7a68adc19c
bishops250 <bishops250@gmail.com>
2019-10-18 11:20:09 -0400

    Working on it...

---
+++ src/NumericComparisons.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NumericComparisons {
+
+
+    public static boolean is_Odd(int num) {
+        return num % 2 != 0;
+    }
+
+    public static void main(String[]args) {
+
+        Scanner user_input = new Scanner(System.in);
+
+        int num = user_input.nextInt();
+
+    }
+
+}

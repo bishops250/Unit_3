fc4119c6677ae152f5643ec77efedda2b03c88ad
bishops250 <bishops250@gmail.com>
2019-10-21 11:57:50 -0400

    working on it again.

--- src/NumericComparisons.java
+++ src/NumericComparisons.java
@@ -5,13 +5,25 @@ public class NumericComparisons {
 
     public static boolean is_Odd(int num) {
         return num % 2 != 0;
+
+    }
+
+    public static boolean is_Even(int num) {
+        return num % 2 != 1 ;
+
     }
 
     public static void main(String[]args) {
 
         Scanner user_input = new Scanner(System.in);
 
+        System.out.println("Please enter the number you would like to be determined as either Odd or even: ");
         int num = user_input.nextInt();
+
+        is_Odd(num);
+
+        System.out.println(num + "is odd:" + is_Odd(num));
+
 
     }
 

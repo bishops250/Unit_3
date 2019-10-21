6b52ad0ed06ddd1d0b7094e9d90c7d1b6c1b5a7c
bishops250 <bishops250@gmail.com>
2019-10-21 12:04:19 -0400

    Added all the methods just need to add code to methods.

--- src/NumericComparisons.java
+++ src/NumericComparisons.java
@@ -3,15 +3,51 @@ import java.util.Scanner;
 public class NumericComparisons {
 
 
-    public static boolean is_Odd(int num) {
+    private static boolean is_Odd(int num) {
         return num % 2 != 0;
 
     }
 
-    public static boolean is_Even(int num) {
+    private static boolean is_Even(int num) {
         return num % 2 != 1 ;
 
     }
+
+    private static boolean is_Positive(int num) {
+
+        return num
+    }
+
+    private static boolean is_Negative(int num) {
+
+        return num
+    }
+
+    private static boolean is_Zero(int num) {
+
+        return num
+    }
+
+    private static boolean is_Non_Positive(int num) {
+
+        return num
+    }
+
+    private static boolean is_Non_Negative(int num) {
+
+        return num
+    }
+
+    private static boolean is_Factor(int num) {
+
+        return num
+    }
+
+    private static boolean is_Multiple(int num) {
+
+        return num
+    }
+
 
     public static void main(String[]args) {
 
@@ -19,6 +55,9 @@ public class NumericComparisons {
 
         System.out.println("Please enter the number you would like to be determined as either Odd or even: ");
         int num = user_input.nextInt();
+
+        System.out.println("Please enter the number you would like to find the multiples and factors of: ");
+        int num2 = user_input.nextInt();
 
         is_Odd(num);
 

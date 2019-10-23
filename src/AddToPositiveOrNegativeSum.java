c75ab763d54ecdbba80d43fb8538faa918d5995f
bishops250 <bishops250@gmail.com>
2019-10-23 14:40:43 -0400

    Done

--- src/AddToPositiveOrNegativeSum.java
+++ src/AddToPositiveOrNegativeSum.java
@@ -3,8 +3,20 @@ import java.util.Scanner;
 public class AddToPositiveOrNegativeSum {
 
     public static void main(String[]args) {
+        Scanner user_input = new Scanner(System.in);
 
-       Scanner user_input = new Scanner(System.in);
+        int sum_positive, sum_negative, num;
+
+                for (int i = 1; i <= 10; i++)        {
+                    /* in this block, allow the user to input a number and add the number to the appropriate sum */
+                }
+                //output the sums    }
+
+
+
+       System.out.println("Please enter the number you would like to find the sum for: ");
+                num = user_input.nextInt();
+
 
 
 

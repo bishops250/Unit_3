c214d98fd53be4117a12afbf8a5cecc24e5be2f7
bishops250 <bishops250@gmail.com>
2019-10-23 14:29:25 -0400

    Done

--- src/FindLargest.java
+++ src/FindLargest.java
@@ -6,5 +6,26 @@ public class FindLargest {
 
         Scanner user_input = new Scanner(System.in);
 
+        System.out.println(" Please enter your first number");
+        int num1= user_input.nextInt();
+
+        System.out.println(" Please enter your second number");
+        int num2= user_input.nextInt();
+
+        if (num1==num2) {
+            System.out.println(num1 + " and " + num2 + " are equal");
+        }
+
+        if (num1 > num2) {
+            System.out.println(num1 + " is larger than " + num2);
+        }
+
+        if (num1 < num2) {
+            System.out.println(num2 + " is larger than " + num1);
+        }
+
+
+
+
     }
 }

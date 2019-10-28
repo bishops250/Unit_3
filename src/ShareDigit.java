e5d6d75917facb4678dad2d2178f761b81a9b7da
bishops250 <bishops250@gmail.com>
2019-10-28 10:15:15 -0400

    Working on it.

---
+++ src/ShareDigit.java
@@ -0,0 +1,30 @@
+
+public class ShareDigit {
+
+    private static Boolean true_digit (int num1, int num2) {
+
+        if () {
+            
+            return true;
+        }
+
+        else if {
+
+            return false;
+        }
+        else if {
+
+            return false;
+        }
+
+    }
+
+
+    public static void main(String[]args) {
+
+        true_digit(12,23);
+        true_digit(12,43);
+        true_digit(12,44);
+
+    }
+}

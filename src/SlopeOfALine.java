aba414c29d33f543aac275a2115ee3f510b5d3e6
bishops250 <bishops250@gmail.com>
2019-10-28 09:55:47 -0400

    Finished SlopeOfALine.

---
+++ src/SlopeOfALine.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SlopeOfALine {
+
+    private static String slope(int x1, int y1, int x2, int y2) {
+
+        double slope = (y2 - y1) / (x2 - x1);
+
+        if (x1 == x2) {
+            return ("The line defined by the points" + "(" + x1 + "," + y1 + ") and" +
+                    "(" + x2 + "," + y2 + ") is a vertical line and the slope is undefined.");
+        } else {
+
+            return "The line defined by the points" + "(" + x1 + "," + y1 + ") and" +
+                    "(" + x2 + "," + y2 + ") has a slope of " + slope;
+        }
+    }
+
+        public static void main (String[]args){
+
+            Scanner user_input = new Scanner(System.in);
+
+            System.out.println(" Please enter the first X coordinate: ");
+            int x1 = user_input.nextInt();
+            System.out.println(" Please enter the first Y coordinate: ");
+            int y1 = user_input.nextInt();
+            System.out.println(" Please enter the second X coordinate: ");
+            int x2 = user_input.nextInt();
+            System.out.println(" Please enter the second Y coordinate: ");
+            int y2 = user_input.nextInt();
+
+            slope(x1, y1, x2, y2);
+        }
+    }

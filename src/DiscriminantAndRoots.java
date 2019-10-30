6f860e0802fab81fa3f782b279829a7f49498d7c
bishops250 <bishops250@gmail.com>
2019-10-30 11:54:23 -0400

    AutoTestAdded

--- src/DiscriminantAndRoots.java
+++ src/DiscriminantAndRoots.java
@@ -1,2 +1,25 @@
 public class DiscriminantAndRoots {
+
+    public static String roots(int a, int b, int c) {
+
+        double root=
+        if(root>0) {
+
+        }
+
+        else if(root<0) {
+
+        }
+
+        else if(root==0) {
+
+            return 
+        }
+
+    }
+
+    public static void main(String[]args) {
+
+    }
+
 }

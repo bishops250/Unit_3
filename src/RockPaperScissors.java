b51b44fd26bb3c63b7cdd9c83305fa6370bd135a
bishops250 <biishops250@gmail.com>
2019-11-07 20:19:27 -0500

    Found Huge Mistake I used== instead of .equals making corrections and including comments, etc.

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -26,6 +26,7 @@ import java.util.Scanner;
 
 public class RockPaperScissors {
 
+
     public static String getUserChoice() {
         Scanner user_input = new Scanner(System.in);
         System.out.println("Please choose either rock, paper, or scissors.");
@@ -34,6 +35,10 @@ public class RockPaperScissors {
 
     }
 
+    /* getComputerChoice(): Randomly Picks a Num between 1 and 3 inclusive, and then it assigns nums a option either
+    the computer chooses "rock",", "paper", or "scissors"-options are not weighted unevenly. It Then returns the answer
+    */
+
     public static String getComputerChoice() {
         double random_chance=(int)Math.random()*3;
         String computer_choice;
@@ -41,6 +46,7 @@ public class RockPaperScissors {
         if(random_chance==1) {
             return computer_choice=("rock");
         }
+
         else if(random_chance==2) {
             return computer_choice=("scissors");
         }
@@ -51,6 +57,9 @@ public class RockPaperScissors {
 
     }
 
+    /* whoWins() decides based on string equality or difference who is the winner or if their is a tie.
+    The variables allow me to only have one return statement and not have to rewrite who wins multiple times.
+     */
     public static String whoWins(String computer, String person) {
         String computer_wins="The computer win!";
         String user_wins="\nYou win!";
@@ -58,38 +67,43 @@ public class RockPaperScissors {
         String the_game_winner;
 
 
-        if (getComputerChoice()==getUserChoice()) {
+        if (computer.equals(person)) {  /*The User and Computer choose the same action.So it sets the Winner
+        as a tie */
            the_game_winner = tie;
         }
 
-        else if (getUserChoice()=="rock" && getComputerChoice()=="scissors" ) { /*User chooses Rock &
+        else if (person.equals("rock") && computer.equals("scissors") ) { /*User chooses Rock &
              Computer chooses Scissors, Winner = User*/
           the_game_winner = user_wins;
         }
 
-        else if(getComputerChoice()=="rock" && getUserChoice()=="scissors" ) {/*User chooses Rock &
-             Computer chooses Scissors, Winner = User*/
+        else if(computer.equals("rock") && person.equals("scissors") ) {/*Computer chooses Rock &
+             Person chooses Scissors, Winner = Computer */
         the_game_winner= computer_wins;
         }
 
 
-        else if(getUserChoice()=="paper" && getComputerChoice()=="scissors" ) {/*User chooses Rock &
-             Computer chooses Scissors, Winner = User*/
-
+        else if(person.equals("paper") && computer.equals("scissors") ) {/*User chooses Paper &
+             Computer chooses Scissors, Winner = computer*/
+                the_game_winner= computer_wins;
             }
 
-            else if(getComputerChoice()=="paper" && getUserChoice()=="scissors" ) {/*User chooses Rock &
-             Computer chooses Scissors, Winner = User*/
-
+            else if(computer.equals("paper") && person.equals("scissors") ) {/*Computer chooses Paper &
+             Person chooses Scissors, Winner = User*/
+            the_game_winner= user_wins;
             }
 
-        else if(getUserChoice()=="paper" && getComputerChoice()== ) {/*User chooses Rock &
+        else if(person.equals("rock") && computer.equals("paper") ) {/*User chooses Rock &
+             Computer chooses Scissors, Winner = User*/
+
+        }
+        else if(person.equals("rock") && computer.equals("paper") ) {/*User chooses Rock &
              Computer chooses Scissors, Winner = User*/
 
         }
 
-        else /*User chooses Rock & Computer chooses Scissors, Winner = User*/
-
+        else/*User chooses Rock & Computer chooses Scissors, Winner = User*/
+            return "Invalid input";
 
             return "You chose " + getComputerChoice()+".\n"+ "The computer chose " + getComputerChoice()
                     +the_game_winner ;
